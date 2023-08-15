77130d79543251c5d470cffc33244472c1e0693e
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-08-15 11:53:15 +0700

    fix(middleware): fix auth middleware

--- src/middleware/Authmiddleware.jsx
+++ src/middleware/Authmiddleware.jsx
@@ -1,13 +1,25 @@
-import { Navigate } from 'react-router';
+import { ToastContainer, toast } from 'react-toastify';
 import Cookies from 'universal-cookie';
-// eslint-disable-next-line react/prop-types
+import optionToast from '../constants/optionToast';
+
 const Authmiddleware = ({ children }) => {
   const cookies = new Cookies()
 
   const token = cookies.get('token')
 
   if (!(token)) {
-    return <Navigate to="/" />
+    toast.error("Sign-in is required to access this page. Please log in or sign up.", optionToast);
+
+    return (
+      <>
+        <ToastContainer />
+        {
+          setTimeout(() => {
+            window.location.replace('/')
+          }, 2000)
+        }
+      </>
+    )
   }
 
   return children
